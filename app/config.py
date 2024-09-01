143e2f0db7bfa4fbbb7f856414e1af736d68dcf9
Sahil <136861381+SahilKarwasra@users.noreply.github.com>
2024-09-01 21:55:22 +0530

    Online MongoDb Connection

--- app/config.py
+++ app/config.py
@@ -1,10 +1,12 @@
 import os
 
-SECRET_KEY = os.getenv(
-    "SECRET_KEY", "9b64bf7e9d2e62d992265f36cb3f3ff3363d8de6005c6d4466dfb9c044e4bc8d"
-)
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+from dotenv import load_dotenv
 
-MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "AuthDB")
+load_dotenv()
+
+
+MONGODB_URL = os.getenv("MONGODB_URL")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
--- app/database.py
+++ app/database.py
@@ -4,3 +4,9 @@ from app.config import DATABASE_NAME, MONGODB_URL
 
 client = AsyncIOMotorClient(MONGODB_URL)
 db = client[DATABASE_NAME]
+
+try:
+    client.admin.command("ping")
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
