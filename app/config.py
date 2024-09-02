88fe5737ee936fc4f055269597abc4e9badb2074
Sahil <136861381+SahilKarwasra@users.noreply.github.com>
2024-09-02 15:51:18 +0530

    trying to fix

--- app/config.py
+++ app/config.py
@@ -5,8 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-MONGODB_URL = os.getenv("MONGODB_URL")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
-SECRET_KEY = os.getenv("SECRET_KEY")
+MONGODB_URL = os.getenv(
+    "MONGODB_URL",
+    "mongodb+srv://Sahil:LaptopOxide@authdb.gksaz.mongodb.net/?retryWrites=true&w=majority&appName=AuthDB",
+)
+DATABASE_NAME = os.getenv("DATABASE_NAME", "AuthDB")
+SECRET_KEY = os.getenv(
+    "SECRET_KEY", "9b64bf7e9d2e62d992265f36cb3f3ff3363d8de6005c6d4466dfb9c044e4bc8d"
+)
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
